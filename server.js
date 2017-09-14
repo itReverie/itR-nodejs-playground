e2807a61791f1c1197b2484cde4a92d3a45c66dd
itreverie <brendadhk@gmail.com>
2017-09-14 15:45:21 -0400

    Adding a jade view engine

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const http= require('http');
 const express = require('express');
 const app= express();
 
-const server= http.createServer(app);
+//Setting up an engine
+app.set("view engine", "jade");
 
+//jade looks by default for views in the folder views
 app.get('/', function(request, response){
-    response.send('hola');
+    response.render('jade/index', {title: "Express + Jade"});
 });
 
-
+//Server
+const server= http.createServer(app);
 server.listen(3000, '127.0.0.1');
