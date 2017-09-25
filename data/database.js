3a4765fa1c603292d4bdfa971cdd67d5b246c535
itreverie <brendadhk@gmail.com>
2017-09-25 13:40:30 -0400

    adding mongodb to read and write data

---
+++ data/database.js
@@ -0,0 +1,36 @@
+(function (database){
+
+    const MongoClient = require('mongodb').MongoClient;
+
+    //url for production
+    let uri = "mongodb://mongodbUser:mongodbPassword@cluster0-shard-00-00-i6dab.mongodb.net:27017,cluster0-shard-00-01-i6dab.mongodb.net:27017,cluster0-shard-00-02-i6dab.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+    //url for development
+    //uri= "mongodb://localhost:27017/theBoard"; //it will create the db
+
+
+    let theDb= null;
+
+    database.getDb = function (next) {
+        //I want to create the db just when I need to
+        if(!theDb)
+        {
+            //Connect to the DB
+            MongoClient.connect(uri, function(err, db) {
+                if(err)
+                {
+                    next(err, null);
+                }
+                else {
+                    //We are wrapping the db in an object in case we need it later with more parameters.
+                    //Maybe we would like to know which user is accessing it, etc.
+                    theDb = {db: db, notes: db.collection("notes")};
+                    next(null, theDb);
+                    //db.close();
+                }
+            });
+        }
+        else{
+            next(null, theDb);
+        }
+    }
+})(module.exports);
--- data/index.js
+++ data/index.js
@@ -1,9 +1,65 @@
 (function(data){
 
-    let seedData = require("./seedData");
+    const seedData = require("./seedData");
+    const database = require("./database");
 
     //We are setting an async operation
     data.getNoteCategories = function (next){
-        next(null,seedData.initialNotes);
+        database.getDb(function(err,db){
+            if(err){
+                console.log('Failed to seed the database:',err);
+            }
+            else {
+                //Find is like where in sql
+                // {name: 'People'}
+                // {notes: {$size : 5 }}
+             db.notes.find().sort({ name: 1}).toArray(function(err, results){
+                 if(err){
+                     console.log('Failed to seed the database:',err);
+                 }
+
+                 next(null, results);
+             });
+            }
+        });
     };
+
+    function seedDataBase(){
+        database.getDb(function(err,db){
+            if(err){
+                console.log('Failed to seed the database:',err);
+            }
+            else {
+                //Test to see if data exists already asynchronously
+                db.notes.count(function (err, count){
+                    if(err){
+                        console.log('Failed to seed the database:',err);
+                    }
+                    else {
+                        if(count === 0)
+                        {
+                            console.log('It is the first time, lets seed the db');
+                            seedData.initialNotes.forEach(function(item)
+                                {
+                                    db.notes.insert(item, function (err){
+                                        if(err){
+                                            console.log("Failed to insert into the db");
+                                        }
+                                    })
+                                }
+                            );
+                        }
+                        else
+                        {
+                            console.log('Db already seeded.');
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    seedDataBase();
+
 })(module.exports);
